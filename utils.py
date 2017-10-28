fbbdced30ac0bb2818b33d712ed7be905b86e5d6
naturomics <naturomics.liao@gmail.com>
2017-10-28 14:06:58 +0800

    Function for loading mnist dataset

---
+++ utils.py
@@ -0,0 +1,27 @@
+import os
+import numpy as np
+
+from config import cfg
+
+
+def load_mnist(path):
+    fd = open(os.path.join(cfg.dataset, 'train-images-idx3-ubyte'))
+    loaded = np.fromfile(file=fd, dtype=np.uint8)
+    trX = loaded[16:].reshape((60000, 28, 28, 1)).astype(np.float)
+
+    fd = open(os.path.join(cfg.dataset, 'train-labels-idx1-ubyte'))
+    loaded = np.fromfile(file=fd, dtype=np.uint8)
+    trY = loaded[8:].reshape((60000)).astype(np.float)
+
+    fd = open(os.path.join(cfg.dataset, 't10k-images-idx3-ubyte'))
+    loaded = np.fromfile(file=fd, dtype=np.uint8)
+    teX = loaded[16:].reshape((10000, 28, 28, 1)).astype(np.float)
+
+    fd = open(os.path.join(cfg.dataset, 't10k-labels-idx1-ubyte'))
+    loaded = np.fromfile(file=fd, dtype=np.uint8)
+    teY = loaded[8:].reshape((10000)).astype(np.float)
+
+    trY = np.asarray(trY)
+    teY = np.asarray(teY)
+
+    return trX, trY, teX, teX
