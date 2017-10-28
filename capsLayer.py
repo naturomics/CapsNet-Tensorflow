4a51c21a104f491625128dd1bf72ba2aea7bcd33
naturomics <naturomics.liao@gmail.com>
2017-10-28 22:10:17 +0800

    Finish the routing algorithm

--- capsLayer.py
+++ capsLayer.py
@@ -1,5 +1,7 @@
 import tensorflow as tf
 
+from config import cfg
+
 
 class CapsConv(object):
     def __init__(self, num_units):
@@ -16,5 +18,54 @@ class CapsConv(object):
 
 
 class Routing(object):
-    def __init__(self, cfg):
-        self.cfg = cfg
+    ''' The routing algorithm.
+    Args:
+        input: A Tensor with shape [batch_size, num_caps, length(u_i)]
+
+    Returns:
+        A Tensor of shape [batch_size, 1, length(v_j), 1] representing the
+        vector output `v_j` of capsule j
+    Notes:
+        u_i represents the vector output of capsule i in the layer l, and
+        v_j the vector output of capsule j in the layer l+1.
+     '''
+
+    def __init__(self):
+        # [batch_size, 32x6x6, 8]
+        self.input = tf.placeholder(tf.float32,
+                                    shape=(cfg.batch_size, 1152, 8, 1))
+        with tf.variable_scope('routing'):
+            self.W_ij = tf.get_variable('w_ij', shape=(1, 1152, 16, 8))
+            self.b_ij = tf.get_variable('b_ij', shape=(1, 1152, 1, 1))
+
+    def __call__(self, input):
+        '''
+        Args:
+            input: shape [batch_size, 1152, 8, 1]
+        Returns:
+            shape [batch_size, 1, 16, 1]
+        '''
+        for r_iter in cfg.iter_routing:
+            # line 4:
+            # [1, 1152, 1, 1]
+            c_i = tf.nn.softmax(self.b_ij, dim=1)
+
+            # line 5:
+            # [16, 8] x [8, 1] => [batch_size, 1152, 16, 1]
+            u_hat = tf.matmul(self.W_ij.T, self.input)
+            # weighting u_hat with c_i in the third dim,
+            # then sum in the second dim, resulting in [batch_size, 1, 16, 1]
+            s_j = tf.reduce_sum(tf.multipy(c_i, u_hat))
+
+            # line 6:
+            # squash using Eq.1, resulting in [batch_size, 1, 16, 1]
+            s_abs = tf.abs(s_j)
+            scalar_factor = tf.square(s_abs) / (1 + tf.square(s_abs))
+            v_j = scalar_factor * tf.divide(s_j, s_abs)
+            # line 7:
+            # [1152, 16] x [16, 1] => [1152, 1], then reduce mean in the
+            # batch_size dim, resulting in [1, 1152, 1, 1]
+            u_produce_v = tf.matmul(u_hat, v_j)
+            self.b_ij = self.b_ij + u_produce_v
+
+        return(v_j)
--- config.py
+++ config.py
@@ -13,6 +13,7 @@ flags.DEFINE_float('m_minus', 0.1, 'the parameter of m minus')
 flags.DEFINE_float('lambda', 0.5, 'down weight of the loss for absent digit classes')
 flags.DEFINE_integer('batch_size', 128, 'batch size')
 flags.DEFINE_integer('epoch', 500, 'epoch')
+flags.DEFINE_integer('iter_routing', 500, 'number of iterations in routing algorithm')
 
 
 ############################
