3bb8461fe920d983735d32d757cd4078b19c48a4
naturomics <naturomics.liao@gmail.com>
2017-10-28 14:08:47 +0800

    Hyper parameters and env setting

--- config.py
+++ config.py
@@ -7,6 +7,17 @@ flags = tf.app.flags
 #    hyper parameters      #
 ############################
 
+# For separate margin loss
+flags.DeFINE_float('m_plus', 0.9, 'the parameter of m plus')
+flags.DeFINE_float('m_minus', 0.1, 'the parameter of m minus')
+flags.DeFINE_float('lambda', 0.5, 'down weight of the loss for absent digit classes')
+
+
+############################
+#   environment setting    #
+############################
+flags.DEFINE_string('dataset', 'data/mnist', 'the path for dataset')
+
 
 cfg = tf.app.flags.FLAGS
 tf.logging.set_verbosity(tf.logging.INFO)
