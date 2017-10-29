69bd3940503867e76eaa738754c816181ac2963b
naturomics <naturomics.liao@gmail.com>
2017-10-29 10:56:57 +0800

    Setup training loop

---
+++ train.py
@@ -0,0 +1,21 @@
+import tensorflow as tf
+
+from config import cfg
+from utils import load_mnist
+from capsNet import CapsNet
+
+
+if __name__ == "__main__":
+    trainX, trainY, testX, testY = load_mnist(cfg.dataset)
+
+    capsNet = CapsNet()
+    tf.logging.info('Graph loaded')
+    sv = tf.train.Supervisor()
+
+    with sv.managed_session() as sess:
+        for epoch in range(cfg.epoch):
+            if sv.should_stop():
+                break
+            sess.run(capsNet.train_op)
+
+    tf.logging.info('Training done')
