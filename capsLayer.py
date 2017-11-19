Fix Routing algorithm. Remove gradient flow in inner iterations.

capsLayer.py
@@ -130,6 +130,9 @@ def routing(input, b_IJ):
     u_hat = tf.matmul(W, input, transpose_a=True)
     assert u_hat.get_shape() == [cfg.batch_size, 1152, 10, 16, 1]
 
+    # In forward, u_hat_stopped = u_hat; in backward, no gradient passed back from u_hat_stopped to u_hat
+    u_hat_stopped = tf.stop_gradient(u_hat, name='stop_gradient')
+
     # line 3,for r iterations do
     for r_iter in range(cfg.iter_routing):
         with tf.variable_scope('iter_' + str(r_iter)):
@@ -137,27 +140,33 @@ def routing(input, b_IJ):
             # => [1, 1152, 10, 1, 1]
             c_IJ = tf.nn.softmax(b_IJ, dim=2)
 
-            # line 5:
-            # weighting u_hat with c_IJ, element-wise in the last two dims
-            # => [batch_size, 1152, 10, 16, 1]
-            s_J = tf.multiply(c_IJ, u_hat)
-            # then sum in the second dim, resulting in [batch_size, 1, 10, 16, 1]
-            s_J = tf.reduce_sum(s_J, axis=1, keep_dims=True)
-            assert s_J.get_shape() == [cfg.batch_size, 1, 10, 16, 1]
+            # At last iteration, use `u_hat` in order to receive gradients from the following graph
+            if r_iter == cfg.iter_routing - 1:
+                # line 5:
+                # weighting u_hat with c_IJ, element-wise in the last two dims
+                # => [batch_size, 1152, 10, 16, 1]
+                s_J = tf.multiply(c_IJ, u_hat)
+                # then sum in the second dim, resulting in [batch_size, 1, 10, 16, 1]
+                s_J = tf.reduce_sum(s_J, axis=1, keep_dims=True)
+                assert s_J.get_shape() == [cfg.batch_size, 1, 10, 16, 1]
 
-            # line 6:
-            # squash using Eq.1,
-            v_J = squash(s_J)
-            assert v_J.get_shape() == [cfg.batch_size, 1, 10, 16, 1]
+                # line 6:
+                # squash using Eq.1,
+                v_J = squash(s_J)
+                assert v_J.get_shape() == [cfg.batch_size, 1, 10, 16, 1]
+            elif r_iter < cfg.iter_routing - 1:  # Inner iterations, do not apply backpropagation
+                s_J = tf.multiply(c_IJ, u_hat_stopped)
+                s_J = tf.reduce_sum(s_J, axis=1, keep_dims=True)
+                v_J = squash(s_J)
 
-            # line 7:
-            # reshape & tile v_j from [batch_size ,1, 10, 16, 1] to [batch_size, 10, 1152, 16, 1]
-            # then matmul in the last tow dim: [16, 1].T x [16, 1] => [1, 1], reduce mean in the
-            # batch_size dim, resulting in [1, 1152, 10, 1, 1]
-            v_J_tiled = tf.tile(v_J, [1, 1152, 1, 1, 1])
-            u_produce_v = tf.matmul(u_hat, v_J_tiled, transpose_a=True)
-            assert u_produce_v.get_shape() == [cfg.batch_size, 1152, 10, 1, 1]
-            if r_iter < cfg.iter_routing - 1:
+                # line 7:
+                # reshape & tile v_j from [batch_size ,1, 10, 16, 1] to [batch_size, 10, 1152, 16, 1]
+                # then matmul in the last tow dim: [16, 1].T x [16, 1] => [1, 1], reduce mean in the
+                # batch_size dim, resulting in [1, 1152, 10, 1, 1]
+                v_J_tiled = tf.tile(v_J, [1, 1152, 1, 1, 1])
+                u_produce_v = tf.matmul(u_hat_stopped, v_J_tiled, transpose_a=True)
+                assert u_produce_v.get_shape() == [cfg.batch_size, 1152, 10, 1, 1]
+
                 # b_IJ += tf.reduce_sum(u_produce_v, axis=0, keep_dims=True)
                 b_IJ += u_produce_v
 