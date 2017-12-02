1121a36eaeedef9eb7a0d400c6c004adb0839c09
naturomics <naturomics.liao@gmail.com>
2017-12-02 20:17:39 +0800

    Fashion-MNIST support

--- utils.py
+++ utils.py
@@ -3,47 +3,94 @@ import scipy
 import numpy as np
 import tensorflow as tf
 
-from config import cfg
 
-
-def load_mnist(path, is_training):
-    fd = open(os.path.join(cfg.dataset, 'train-images-idx3-ubyte'))
-    loaded = np.fromfile(file=fd, dtype=np.uint8)
-    trX = loaded[16:].reshape((60000, 28, 28, 1)).astype(np.float)
-
-    fd = open(os.path.join(cfg.dataset, 'train-labels-idx1-ubyte'))
-    loaded = np.fromfile(file=fd, dtype=np.uint8)
-    trY = loaded[8:].reshape((60000)).astype(np.int32)
-
-    fd = open(os.path.join(cfg.dataset, 't10k-images-idx3-ubyte'))
-    loaded = np.fromfile(file=fd, dtype=np.uint8)
-    teX = loaded[16:].reshape((10000, 28, 28, 1)).astype(np.float)
-
-    fd = open(os.path.join(cfg.dataset, 't10k-labels-idx1-ubyte'))
-    loaded = np.fromfile(file=fd, dtype=np.uint8)
-    teY = loaded[8:].reshape((10000)).astype(np.int32)
-
-    # normalization and convert to a tensor [60000, 28, 28, 1]
-    trX = tf.convert_to_tensor(trX / 255., tf.float32)
-
-    # => [num_samples, 10]
-    # trY = tf.one_hot(trY, depth=10, axis=1, dtype=tf.float32)
-    # teY = tf.one_hot(teY, depth=10, axis=1, dtype=tf.float32)
-
+def load_mnist(batch_size, is_training=True):
+    path = os.path.join('data', 'mnist')
     if is_training:
-        return trX, trY
+        fd = open(os.path.join(path, 'train-images-idx3-ubyte'))
+        loaded = np.fromfile(file=fd, dtype=np.uint8)
+        trainX = loaded[16:].reshape((60000, 28, 28, 1)).astype(np.float32)
+
+        fd = open(os.path.join(path, 'train-labels-idx1-ubyte'))
+        loaded = np.fromfile(file=fd, dtype=np.uint8)
+        trainY = loaded[8:].reshape((60000)).astype(np.int32)
+
+        trX = trainX[:55000] / 255.
+        trY = trainY[:55000]
+
+        valX = trainX[55000:, ] / 255.
+        valY = trainY[55000:]
+
+        num_tr_batch = 55000 // batch_size
+        num_val_batch = 5000 // batch_size
+
+        return trX, trY, num_tr_batch, valX, valY, num_val_batch
     else:
-        return teX / 255., teY
+        fd = open(os.path.join(path, 't10k-images-idx3-ubyte'))
+        loaded = np.fromfile(file=fd, dtype=np.uint8)
+        teX = loaded[16:].reshape((10000, 28, 28, 1)).astype(np.float)
+
+        fd = open(os.path.join(path, 't10k-labels-idx1-ubyte'))
+        loaded = np.fromfile(file=fd, dtype=np.uint8)
+        teY = loaded[8:].reshape((10000)).astype(np.int32)
+
+        num_te_batch = 10000 // batch_size
+        return teX / 255., teY, num_te_batch
 
 
-def get_batch_data():
-    trX, trY = load_mnist(cfg.dataset, cfg.is_training)
+def load_fashion_mnist(batch_size, is_training=True):
+    path = os.path.join('data', 'fashion-mnist')
+    if is_training:
+        fd = open(os.path.join(path, 'train-images-idx3-ubyte'))
+        loaded = np.fromfile(file=fd, dtype=np.uint8)
+        trainX = loaded[16:].reshape((60000, 28, 28, 1)).astype(np.float32)
 
+        fd = open(os.path.join(path, 'train-labels-idx1-ubyte'))
+        loaded = np.fromfile(file=fd, dtype=np.uint8)
+        trainY = loaded[8:].reshape((60000)).astype(np.int32)
+
+        trX = trainX[:55000] / 255.
+        trY = trainY[:55000]
+
+        valX = trainX[55000:, ] / 255.
+        valY = trainY[55000:]
+
+        num_tr_batch = 55000 // batch_size
+        num_val_batch = 5000 // batch_size
+
+        return trX, trY, num_tr_batch, valX, valY, num_val_batch
+    else:
+        fd = open(os.path.join(path, 't10k-images-idx3-ubyte'))
+        loaded = np.fromfile(file=fd, dtype=np.uint8)
+        teX = loaded[16:].reshape((10000, 28, 28, 1)).astype(np.float)
+
+        fd = open(os.path.join(path, 't10k-labels-idx1-ubyte'))
+        loaded = np.fromfile(file=fd, dtype=np.uint8)
+        teY = loaded[8:].reshape((10000)).astype(np.int32)
+
+        num_te_batch = 10000 // batch_size
+        return teX / 255., teY, num_te_batch
+
+
+def load_data(dataset, batch_size, is_training=True, one_hot=False):
+    if dataset == 'mnist':
+        return load_mnist(batch_size, is_training)
+    elif dataset == 'fashion-mnist':
+        return load_fashion_mnist(batch_size, is_training)
+    else:
+        raise Exception('Invalid dataset, please check the name of dataset:', dataset)
+
+
+def get_batch_data(dataset, batch_size, num_threads):
+    if dataset == 'mnist':
+        trX, trY, num_tr_batch, valX, valY, num_val_batch = load_mnist(batch_size, is_training=True)
+    elif dataset == 'fashion-mnist':
+        trX, trY, num_tr_batch, valX, valY, num_val_batch = load_fashion_mnist(batch_size, is_training=True)
     data_queues = tf.train.slice_input_producer([trX, trY])
-    X, Y = tf.train.shuffle_batch(data_queues, num_threads=cfg.num_threads,
-                                  batch_size=cfg.batch_size,
-                                  capacity=cfg.batch_size * 64,
-                                  min_after_dequeue=cfg.batch_size * 32,
+    X, Y = tf.train.shuffle_batch(data_queues, num_threads=num_threads,
+                                  batch_size=batch_size,
+                                  capacity=batch_size * 64,
+                                  min_after_dequeue=batch_size * 32,
                                   allow_smaller_final_batch=False)
 
     return(X, Y)
@@ -69,9 +116,3 @@ def mergeImgs(images, size):
         imgs[j * h:j * h + h, i * w:i * w + w, :] = image
 
     return imgs
-
-
-if __name__ == '__main__':
-    X, Y = load_mnist(cfg.dataset, cfg.is_training)
-    print(X.get_shape())
-    print(X.dtype)
