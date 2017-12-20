f5dcdea9422b7b8eda0035ebd802f35df3e8bf38
naturomics <naturomics.liao@gmail.com>
2017-12-20 19:12:06 +0800

    Fix prompt info for training progress

--- main.py
+++ main.py
@@ -50,8 +50,7 @@ def train(model, supervisor, num_label):
     with supervisor.managed_session(config=config) as sess:
         print("\nNote: all of results will be saved to directory: " + cfg.results)
         for epoch in range(cfg.epoch):
-            sys.stdout.write('Training for epoch ' + str(epoch) + '/' + str(cfg.epoch) + ':')
-            sys.stdout.flush()
+            print('Training for epoch ' + str(epoch) + '/' + str(cfg.epoch) + ':')
             if supervisor.should_stop():
                 print('supervisor stoped!')
                 break
