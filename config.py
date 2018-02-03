fac40852d416b59d585c36273e0eeb6eb3e61273
Naman Bhalla <namanbhalla1998@gmail.com>
2018-02-03 19:47:19 +0530

    Fix typo
    exampls -> examples

--- config.py
+++ config.py
@@ -27,7 +27,7 @@ flags.DEFINE_float('regularization_scale', 0.392, 'regularization coefficient fo
 ############################
 flags.DEFINE_string('dataset', 'mnist', 'The name of dataset [mnist, fashion-mnist')
 flags.DEFINE_boolean('is_training', True, 'train or predict phase')
-flags.DEFINE_integer('num_threads', 8, 'number of threads of enqueueing exampls')
+flags.DEFINE_integer('num_threads', 8, 'number of threads of enqueueing examples')
 flags.DEFINE_string('logdir', 'logdir', 'logs directory')
 flags.DEFINE_integer('train_sum_freq', 100, 'the frequency of saving train summary(step)')
 flags.DEFINE_integer('val_sum_freq', 500, 'the frequency of saving valuation summary(step)')
